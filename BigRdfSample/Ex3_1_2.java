6a2e6ed832592a038f5450553b142aafac2e7d18
Kouji Kozaki <kozaki@osakac.ac.jp>
2025-08-08 15:34:44 +0900

    Add files via upload

---
+++ BigRdfSample/Ex3_1_2.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.sparql.exec.http.QueryExecutionHTTP;
+
+
+/* SPARQL Endpoint に対するクエリ例
+ * 注）Proxyの設定が必要な環境で実行するときは，実行時のJVMのオプションとして
+ *      -DproxySet=true -DproxyHost=wwwproxy.osakac.ac.jp -DproxyPort=8080
+ *     を追加する，
+ *     Eclipseの場合「実行の構成＞引数」で設定可能
+ * /
+ */
+
+public class Ex3_1_2 {
+
+	static public void main(String[] args) throws FileNotFoundException{
+
+		//クエリの作成
+		String queryStr = """
+				PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+				PREFIX wd: <http://www.wikidata.org/entity/>
+				PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+				
+				select DISTINCT ?s ?lbl ?code
+				where{
+				  ?s wdt:P31 wd:Q50337.
+				  ?s wdt:P429 ?code.
+				  ?s rdfs:label ?lbl.
+				  FILTER(lang(?lbl)="ja")
+				  FILTER(STRSTARTS(?lbl,"大阪"))
+				}
+				""";
+		
+		try{
+			  //入力するファイルの設定
+			  File file = new File("pref.txt");
+			  BufferedReader br = new BufferedReader(new FileReader(file));
+
+			//出力するファイルの設定
+			  File file_out = new File("output3-2_pref.txt");
+			  FileOutputStream out = new FileOutputStream(file_out);
+
+			  String str;
+			  
+			  while((str = br.readLine()) != null){
+				  
+				  	System.out.println(str);
+					Query query = QueryFactory.create(queryStr.replace("大阪",str));
+
+					 // Remote execution.
+			        try ( QueryExecution qExec = QueryExecutionHTTP.create()
+			                    .endpoint("https://query.wikidata.org/sparql")
+			                    .query(query)
+			                    .param("timeout", "10000")
+			                    .build() ) {
+			        		
+			            // Execute.
+			            ResultSet rs = qExec.execSelect();
+			            
+			            //ResultSetFormatter.out(System.out, rs, query);		//表形式で，
+			            ResultSetFormatter.outputAsCSV(out, rs);
+			        }
+				  
+			  }
+			  
+			  br.close();
+			  out.close();
+
+		  }catch(Exception e){
+			  System.out.println(e);
+		  }
+        
+        }
+     }
